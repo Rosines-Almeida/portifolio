66c54339351a072f2c373678e12d88bae315d256
Rosinês da Silva Almeida <r.da.silva.almeida@avanade.com>
2022-01-18 22:36:40 -0300

    start projeto

---
+++ src/router.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Contato from './views/Contato.vue'
+import Cursos from './views/Cursos.vue'
+import Curso from './views/Curso.vue'
+import Aula from './views/Aula.vue'
+import Projeto from './views/Projeto.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/contato",
+      name: "contato",
+      component: Contato
+    },
+    {
+      path: "/cursos",
+      name: "cursos",
+      component: Cursos
+    },
+    {
+      path: "/projeto",
+      name: "projeto",
+      component: Projeto
+    },
+    {
+      path: "/cursos/:curso",
+      name: "curso",
+      component: Curso,
+      props: true,
+      children: [
+        {
+          path: ":aula",
+          name: "aula",
+          component: Aula,
+          props: true,
+        }
+      ]
+    },
+  ]
+})
